613655d49e63d9abf9801712235529fe84b74983
Zhaonian Luan <zhaonial@uci.edu>
2018-06-25 21:48:04 -0700

    working on invite and accept

--- app/src/main/java/com/auroracatcher/tictactoe/OnlineActivity.java
+++ app/src/main/java/com/auroracatcher/tictactoe/OnlineActivity.java
@@ -1,5 +1,6 @@
 package com.auroracatcher.tictactoe;
 
+import android.graphics.Color;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -15,18 +16,26 @@ import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
 
 public class OnlineActivity extends AppCompatActivity {
 
     EditText userEmail;
     EditText friendEmail;
     Button loginButton;
+    Button inviteButton;
+    Button acceptButton;
 
     private FirebaseAnalytics mFirebaseAnalytics;
     private FirebaseAuth mAuth;
     private String myEmail;
+    private String myId;
 
     // Write a message to the database
     FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -39,6 +48,8 @@ public class OnlineActivity extends AppCompatActivity {
         userEmail = (EditText) findViewById(R.id.userEmail);
         friendEmail = (EditText) findViewById(R.id.friendEmail);
         loginButton = (Button) findViewById(R.id.loginButton);
+        inviteButton = (Button) findViewById(R.id.inviteButton);
+        acceptButton = (Button) findViewById(R.id.acceptButton);
         // Obtain the FirebaseAnalytics instance.
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         mAuth = FirebaseAuth.getInstance();
@@ -63,6 +74,7 @@ public class OnlineActivity extends AppCompatActivity {
                             FirebaseUser user = mAuth.getCurrentUser();
                             if (user != null) {
                                 myEmail = user.getEmail();
+                                myId = user.getUid();
                                 loginButton.setEnabled(false);
                                 userEmail.setText(myEmail);
 
@@ -92,6 +104,40 @@ public class OnlineActivity extends AppCompatActivity {
 
     public void inviteClick(View view) {
         Log.d("invite", friendEmail.getText().toString());
+        myRef.child("Users").child(getUserBeforeAt(friendEmail.getText().toString())).child("Request").push().setValue(myEmail);
+    }
+
+    public void incomingRequestListener() {
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+
+                try {
+                    HashMap<String, Object> td = (HashMap<String, Object>) dataSnapshot.getValue();
+                    if (td != null) {
+                        String value;
+                        for (String key : td.keySet()) {
+                            value = (String) td.get(key);
+                            friendEmail.setText(value);
+                            inviteButton.setBackgroundColor(Color.GREEN);
+                            myRef.child("Users").child(getUserBeforeAt(myEmail)).child("Request").setValue(myId);
+                            break; // TODO: why break?
+                        }
+                    }
+                } catch (Exception ex) {
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("request", "Failed to read value.", error.toException());
+            }
+        });
     }
 
     public void acceptClick(View view) {
